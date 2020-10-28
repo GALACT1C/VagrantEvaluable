e61a519e37628069d37aa27ad23c15be2e31cc2b
Carlos <carlostamarit18@gmail.com>
2020-10-28 23:21:50 +0100

    Subo actividad 20
    He modificado el fichero default para que pille la ruta de la carpeta vagrant y lo sobreescribo en la ruta original. Ahora no es necesario el enlace anterior.

--- provision.sh
+++ provision.sh
@@ -2,8 +2,12 @@ apt-get -y update
 
 apt-get -y install nginx
 
+rm -rf /etc/nginx/sites-enabled
+
+cp -r /vagrant/sites-enabled /etc/nginx
+
 cp -r /usr/share/nginx/www /vagrant
 
 rmdir -r /usr/share/nginx/www
 
-ln -s /vagrant/www /usr/share/nginx
+service nginx start
